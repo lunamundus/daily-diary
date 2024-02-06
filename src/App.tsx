939627dedf64779639f0dcbbd8912668911d64d3
Byeongkuk Oh <lunamundus1007@gmail.com>
2024-02-07 02:48:14 +0900

    로그인 페이지 UI 구성 (진행 중)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,55 @@
+// about React
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-const router = createBrowserRouter([]);
+// about Styled Components
+import { createGlobalStyle, styled } from "styled-components";
+import reset from "styled-reset";
+
+// about Pages
+import MainPage from "./pages/MainPage";
+import LoginPage from "./pages/LoginPage";
+import CreateAccountPage from "./pages/CreateAccountPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
+
+const GlobalStyles = createGlobalStyle`
+${reset}
+
+body {
+  background-color: #e8e8e8;
+  color: #212121;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccountPage />,
+  },
+  {
+    path: "/reset-password",
+    element: <ResetPasswordPage />,
+  },
+]);
 
 function App() {
   return (
-    <>
+    <Wrapper>
+      <GlobalStyles />
       <RouterProvider router={router} />
-    </>
+    </Wrapper>
   );
 }
 
---
+++ src/pages/ResetPasswordPage.tsx
@@ -0,0 +1,9 @@
+function ResetPasswordPage() {
+  return (
+    <>
+      <h1>비밀번호 초기화 페이지입니다.</h1>
+    </>
+  );
+}
+
+export default ResetPasswordPage;
