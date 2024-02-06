4b97a1e649c810721e2eb474a8ebf4bdcf993e61
Byeongkuk Oh <lunamundus1007@gmail.com>
2024-02-07 01:36:29 +0900

    기본 페이지 세팅

---
+++ src/pages/CreateAccountPage.tsx
@@ -0,0 +1,9 @@
+function CreateAccountPage() {
+  return (
+    <>
+      <h1>계정 생성 페이지입니다.</h1>
+    </>
+  );
+}
+
+export default CreateAccountPage;
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+function LoginPage() {
+  return (
+    <>
+      <h1>로그인 페이지입니다.</h1>
+    </>
+  );
+}
+
+export default LoginPage;
